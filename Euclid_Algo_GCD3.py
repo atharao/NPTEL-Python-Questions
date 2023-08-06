7bb0e8884071d9dc33e5ac7ca486de6dbbdd675a
raoathar00786 <raoathar.200@gmail.com>
2023-08-07 01:02:19 +0530

    first Week

---
+++ Euclid_Algo_GCD3.py
@@ -0,0 +1,14 @@
+def GCD(m, n):
+    if m<n:
+        (m,n)=(n,m)
+    
+    while (m%n)!=0:
+
+        return(GCD(n,m%n))
+
+    return(print("The GCD is:",n))
+m=int(input("Enter the First number:"))
+n=int(input("Enter the Second number:"))
+
+GCD(m,n)    
+
