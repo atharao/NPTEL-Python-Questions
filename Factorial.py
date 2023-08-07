8287d26b9379a765cc9b8c75cf94a37f6b7eb7f4
raoathar00786 <raoathar.200@gmail.com>
2023-08-08 03:24:41 +0530

    Second Week

--- Factorial.py
+++ Factorial.py
@@ -1,10 +1,15 @@
-num = int(input("Enter a number: "))    
-factorial = 1    
-if num < 0:    
-   print(" Factorial does not exist for negative numbers")    
-elif num == 0:    
-   print("The factorial of 0 is 1")    
-else:    
-   for i in range(1,num + 1):    
-       factorial = factorial*i    
-   print("The factorial of",num,"is",factorial)    
+def factorial(x):
+
+    if x==1:
+        return(x)
+    
+    else:
+        ans=1
+        while(x>0):
+            ans=ans*x
+            x-=1
+        
+    return(print(ans))
+
+value=int(input("Enter the Number:"))
+factorial(value)
