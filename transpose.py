095f44187a1ef37f1f7c051686a53160862d564a
raoathar00786 <raoathar.200@gmail.com>
2023-08-20 00:54:24 +0530

    Fourth Week

--- transpose.py
+++ transpose.py
@@ -1,17 +1,14 @@
 def transpose(m):
-    num_rows = len(m)
-    num_cols = len(m[0])
-    
     transposed = []
-    for j in range(num_cols):
+    for j in range(len(m[0])):
         transposed_row = []
-        for i in range(num_rows):
+        for i in range(len(m)):
             transposed_row.append(m[i][j])
         transposed.append(transposed_row)
     
     return transposed
-    
-transpose(m)
+m=[[2]]  
+print(transpose(m))
 
 
 
