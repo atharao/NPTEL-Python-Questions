35f0b6ac57c94f6499b330c0797701cb4a0686e7
raoathar00786 <raoathar.200@gmail.com>
2023-08-20 00:54:31 +0530

    Fourth Week

---
+++ sum_square.py
@@ -0,0 +1,13 @@
+def sumsquare(l):
+    sum_odd=0
+    sum_even=0
+
+    for i in l:
+        if i%2!=0:
+            sum_odd=sum_odd+i*i
+        else:
+            sum_even=sum_even+i*i   
+
+    return([sum_odd,sum_even])
+l=[-1,-2,3,7]
+print(sumsquare(l))
